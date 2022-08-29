64d5ff7c79cc0ca3f4e9106205e32e7443e9c6bb
phinic925 <phinicnyangate480@gmail.com>
2022-08-29 12:13:25 +0100

    commit tree 07f00732f7fc4a105353af350d46d93d84bd57e2

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,11 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
+    def full_name
+        "#{first_name} #{last_name}"
+    end
+    def list_roles
+        characters.all.map{|c| "#{c.name} - #{c.show.name}"}
+    end
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+        actors.map{|a| "#{a.first_name} #{a.last_name}"}
+    end
 end
